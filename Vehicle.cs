e466075a2065714ab2fb0cd4661e69f96c5f2be7
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2021-09-22 19:06:55 +0400

    Создание класса Vehicle на базе интерфейса

---
+++ Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace WindowsFormsCruiser
+{
+    public abstract class Vehicle : ITransport
+    {
+        protected float _startPosX;
+        protected float _startPosY;
+        protected int _pictureWidth;
+        protected int _pictureHeight;
+        
+        public int MaxSpeed { protected set; get; }
+        public float Weight { protected set; get; }
+        public Color MainColor { protected set; get; }
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            // Продумать логику
+        }
+        public abstract void DrawCruiser(Graphics g);
+        public abstract void MoveCruiser(Direction direction);
+    }
+}
