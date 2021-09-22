0314674235c43c6a4cdc4e4cdfb3f633d8921b54
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2021-09-22 11:40:43 +0400

    Работа с формой и pictureBox

---
+++ FormCruiser.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WindowsFormsCruiser
+{
+    partial class FormCruiser
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCruiser = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCruiser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCruiser
+            // 
+            this.pictureBoxCruiser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxCruiser.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCruiser.Name = "pictureBoxCruiser";
+            this.pictureBoxCruiser.Size = new System.Drawing.Size(884, 462);
+            this.pictureBoxCruiser.TabIndex = 0;
+            this.pictureBoxCruiser.TabStop = false;
+            // 
+            // FormCruiser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 462);
+            this.Controls.Add(this.pictureBoxCruiser);
+            this.Name = "FormCruiser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Крейсер";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCruiser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCruiser;
+    }
+}
+
--- FormCruiser.cs
+++ FormCruiser.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsCruiser
 {
-    public partial class Form1 : Form
+    public partial class FormCruiser : Form
     {
-        public Form1()
+        public FormCruiser()
         {
             InitializeComponent();
         }
