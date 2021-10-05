08aa350019b1f0beb06170a97dec8a5e234fccf1
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2021-10-05 18:31:05 +0400

    Параметризованный класс Dock

---
+++ Dock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsCruiser
+{
+    public class Dock<T> where T : class, ITransport
+    {
+        private readonly T[] _places;
+        private readonly int pictureWidth;
+        private readonly int pictureHeight;
+        // изменить размеры мест
+        private readonly int _placeSizeWidth = 210;
+        private readonly int _placeSizeHeight = 80;
+
+        public Dock(int picWidth, int picHeight)
+        {
+            int width = picWidth / _placeSizeWidth;
+            int height = picHeight / _placeSizeHeight;
+            _places = new T[width * height];
+            pictureWidth = picWidth;
+            pictureHeight = picHeight;
+        }
+
+        public static bool operator +(Dock<T> p, T ship)
+        {
+            // Прописать логику для сложения
+            return true;
+        }
+
+        public static T operator -(Dock<T> p, int index)
+        {
+            // Прописать логику для вычитания
+            return null;
+        }
+
+        public void Draw(Graphics g)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < _places.Length; i++)
+            {
+                _places[i]?.DrawTransport(g);
+            }
+        }
+
+        private void DrawMarking(Graphics g)
+        {
+            // свой рисунок
+            Pen pen = new Pen(Color.Black, 3);
+            for (int i = 0; i < pictureWidth / _placeSizeWidth; i++)
+            {
+                for (int j = 0; j < pictureHeight / _placeSizeHeight + 1; ++j)
+                {//линия рамзетки места
+                    g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight, i *
+                   _placeSizeWidth + _placeSizeWidth / 2, j * _placeSizeHeight);
+                }
+                g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth,
+               (pictureHeight / _placeSizeHeight) * _placeSizeHeight);
+            }
+        }
+    }
+}
